b261fc0b4823f3f96daba84efba5d3c169917ddf
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-11-08 11:25:54 -0300

    Creating other cards and do some refactoring

--- scripts.js
+++ scripts.js
@@ -1,29 +1,28 @@
-const createCard = (date,time) => {
+const createGame = (country, time) => {
+  return `
+    <li>
+        <img src="images/brazil-flag.svg" alt="Bandeira-Brasil" />
+        <strong>${time}</strong>
+        <img src="images/${country}-flag.svg" alt="Bandeira-${country}" />
+    </li>
+    `;
+};
+
+const createCard = (date, day, games) => {
   return `
     <div class="card">
         <h2>
             ${date}
-            <span>${time}</span>
+            <span>${day}</span>
         </h2>
           <ul>
-            <li>
-              <img src="images/brazil-flag.svg" alt="Bandeira-Brasil" />
-              <strong>16:00</strong>
-              <img src="images/serbia-flag.svg" alt="Bandeira-Sérvia" />
-            </li>
+            ${games}
           </ul>
     </div>
  `;
 };
 
-document.querySelector(".container").innerHTML = `
-    <header>
-      <img src="images/logo.svg" alt="Logo da NLW-Rocketseat" />
-    </header>
-
-    <section class="cards">
-      ${createCard('24/11', 'quinta')}
-      ${createCard('28/11', 'segunda')}
-      ${createCard('02/12', 'sexta')}
-    </section>
-`;
+document.querySelector(".cards").innerHTML =
+  createCard("24/11", "quinta", createGame("serbia", "16:00")) +
+  createCard("28/11", "segunda", createGame("switzerland", "13:00")) +
+  createCard("02/12", "sexta", createGame("cameroon", "16:00"));
